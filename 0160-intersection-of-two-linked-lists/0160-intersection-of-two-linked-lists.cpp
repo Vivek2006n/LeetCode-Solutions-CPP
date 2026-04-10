9a1dd9d952fe3d31fd9a585ffaa99144f331189b
Vivek Negi <vivek.negi.cs@gmail.com>
2026-04-10 22:57:24 +0530

    Time: 73 ms (17.76%), Space: 29.8 MB (8.77%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_map<ListNode*,int>mp;
+        ListNode *tempA = headA;
+        ListNode *tempB = headB;
+
+        while(tempA!=NULL){
+            mp[tempA] = 1;
+            tempA = tempA->next;
+        }
+        while(tempB!=NULL){
+            if(mp.find(tempB)!=mp.end()){
+                return tempB;
+            }
+            tempB = tempB->next;
+        }
+        return NULL;
+    }
+};
