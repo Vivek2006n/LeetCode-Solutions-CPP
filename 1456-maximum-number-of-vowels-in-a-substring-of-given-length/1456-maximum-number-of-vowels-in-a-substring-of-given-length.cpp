ef6636bbf3f782286504f71a5bbe242010e0f1f1
Vivek Negi <vivek.negi.cs@gmail.com>
2026-06-24 22:13:48 +0530

    Time: 10 ms (27.9%), Space: 13.1 MB (59.61%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int windowSum = 0;
         int maxCount = 0;
         int count = 0;
 
@@ -10,7 +9,6 @@ public:
                 count++;
             }
             maxCount = count;
-            windowSum += s[i];
         }
         for(int i=k;i<s.size();i++){
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
@@ -19,8 +17,6 @@ public:
             if(s[i-k]=='a' || s[i-k]=='e' || s[i-k]=='i' || s[i-k]=='o' || s[i-k]=='u'){
                 count--;
             }
-            windowSum += s[i];
-            windowSum -= s[i-k];
             maxCount = max(maxCount,count);
         }
         return maxCount;
