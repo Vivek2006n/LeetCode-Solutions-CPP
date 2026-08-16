c2440439749f180d986bcb014141eb1bc3086f9e
Vivek Negi <vivek.negi.cs@gmail.com>
2026-08-16 22:33:52 +0530

    Time: 0 ms (100%), Space: 24 MB (19.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int insertPos = 0;
+        int j = 0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                nums[insertPos]=nums[i];
-                insertPos++;
+                swap(nums[i],nums[j]);
+                j++;
             }
-        }
-        while(insertPos<nums.size()){
-            nums[insertPos++] = 0;
         }
     }
 };
